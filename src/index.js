28759d88b2f676beb5a0bbd45dc0c97b01393fe1
Jesse Mavi <jesse.mavi@gmail.com>
2016-12-22 17:02:32 -0600

    Makes requests from Giphy API and displays gifs.

--- src/index.js
+++ src/index.js
@@ -2,31 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GifList from './components/GifList'
 import SearchBar from './components/SearchBar';
+import request from 'superagent';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      gifs: [
-        {
-          id: 1,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 2,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 3,
-          url: 'http://fakeimg.pl/300/'
-        }        
-      ]
+      gifs: []
     };
+    // explicitly tell our app that the lexical this of the handleTermChange method is bound to App, not onTermChange. Can instead use fat-arrow functions in handleTermChange
+    this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   handleTermChange(term) {
-    console.log(term);
+    // using regex to replace spaces(multiple terms) with a '+' as the seperator before sending to Giphy API
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+
+    request.get(url, (err, res) => {
+      if(err) {
+        console.log(err);
+      }
+      console.log(res.body.data[0]);
+      this.setState({gifs: res.body.data});
+    });
   }
 
   render() {
