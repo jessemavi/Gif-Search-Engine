97d4497cdc7ec19ea9700610773f9fea24c5613c
Jesse Mavi <jesse.mavi@gmail.com>
2016-12-22 00:11:21 -0600

    Adds SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+  }
+
+  onInputChange(term) {
+    this.setState({
+      term: term
+    });
+    this.props.onTermChange(term);
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <input onChange={event => this.onInputChange(event.target.value)} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
